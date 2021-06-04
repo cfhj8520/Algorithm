fc18320978cfba22bff1f5bf7fcb9f2a9ad217a7
cfhj8520 <32195300+cfhj8520@users.noreply.github.com>
2021-06-04 17:48:43 +0900

    Create 모든아나그램찾기.java
    Hash, sliding window

---
+++ src/모든아나그램찾기.java
@@ -0,0 +1,74 @@
+package coding_test;
+
+import java.util.*;
+  
+public class Main {
+	public static int solution(String S, String T) {
+		int answer = 0;
+		int S_len = S.length();
+		int T_len = T.length();
+		
+		Map<Character,Integer> map = new HashMap<Character,Integer>();
+		Map<Character,Integer> sol = new HashMap<Character,Integer>();
+		char[] arr = new char[S.length()];
+		char[] brr = new char[T.length()];
+		
+		arr = S.toCharArray();
+		brr = T.toCharArray();
+		
+		for(int i=0;i<T_len;i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+			sol.put(brr[i], sol.getOrDefault(brr[i], 0)+1);
+		}
+		
+//		System.out.println("map : "+map);
+//		System.out.println("sol : "+sol);
+		
+		boolean tmp =true;
+		for(char x : sol.keySet()) {
+			if(sol.get(x)!=map.get(x)) {
+				tmp=false;
+			}
+		}
+		if(tmp)
+			answer++;
+		
+		tmp=true;
+		
+		for(int i=0;i<S_len-T_len;i++) {
+			if(map.get(arr[i])==1) {
+				map.remove(arr[i]);
+			}else {
+				map.replace(arr[i], map.get(arr[i])-1);
+			}
+			
+			map.put(arr[i+T_len], map.getOrDefault(arr[i+T_len], 0)+1);
+			
+//			System.out.println("map : "+map);
+			
+			for(char x : sol.keySet()) {
+				if(sol.get(x)!=map.get(x)) {
+					tmp=false;
+				}
+			}
+			if(tmp)
+				answer++;
+			
+			tmp=true;
+		}
+		
+		return answer;
+	}
+	
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    
+    String S = in.next();
+    String T = in.next();
+ 
+    System.out.println(solution(S,T));
+    
+    
+    return ;
+  }
+}
